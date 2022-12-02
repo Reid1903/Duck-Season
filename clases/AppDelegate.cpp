877dce5785ab88e858691dd18f543dd8dd9a42b5
Reid1903 <115179291+Reid1903@users.noreply.github.com>
2022-12-02 15:38:48 -0600

    Add files via upload

---
+++ clases/AppDelegate.cpp
@@ -0,0 +1,92 @@
+
+#include "AppDelegate.h"
+#include "MenuInicio.h"
+#include "HelloWorldScene.h"
+// #define USE_AUDIO_ENGINE 1
+
+#if USE_AUDIO_ENGINE
+#include "audio/include/AudioEngine.h"
+using namespace cocos2d::experimental;
+#endif
+
+USING_NS_CC;
+
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+
+AppDelegate::AppDelegate()
+{
+    //No le muevas padrino
+}
+
+AppDelegate::~AppDelegate() 
+{
+#if USE_AUDIO_ENGINE
+    AudioEngine::end();
+#endif
+}
+
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+// if you want to use the package manager to install more packages,  
+// don't modify or remove this function
+static int register_all_packages()
+{
+    return 0; //flag for packages manager
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        
+        glview = GLViewImpl::createWithFullScreen("Duck Hunting");
+        
+        director -> setOpenGLView(glview);
+    }
+
+
+    director->setDisplayStats(false);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0f / 60);
+
+   
+    // create a scene. it's an autorelease object
+   
+
+    // run
+    auto scene = MenuInicio::createScene();
+    director->runWithScene(scene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
+void AppDelegate::applicationDidEnterBackground() {
+    Director::getInstance()->stopAnimation();
+
+#if USE_AUDIO_ENGINE
+    AudioEngine::pauseAll();
+#endif
+}
+
+
+void AppDelegate::applicationWillEnterForeground() {
+    Director::getInstance()->startAnimation();
+
+#if USE_AUDIO_ENGINE
+    AudioEngine::resumeAll();
+#endif
+}
